79008089b442269f073ab234af948ebd1a6093f1
JonasRingeis <jonas.ringeis@otto.de>
2025-04-29 10:17:07 +0200

    Made SplashScreen load MainWindow

--- Installer/View/Windows/SplashScreen.xaml.cs
+++ Installer/View/Windows/SplashScreen.xaml.cs
@@ -1,11 +1,30 @@
 using System.Windows;
+using Wpf.Ui.Appearance;
 
 namespace Installer.View.Windows;
 
-public partial class SplashScreen
+public partial class SplashScreen : Window
 {
     public SplashScreen()
     {
         InitializeComponent();
+        ApplicationThemeManager.Apply(this);
+
+        _ = LoadMainWindow();
+    }
+
+    private async Task LoadMainWindow()
+    {
+        // mock delay
+        await Task.Delay(2000);
+        
+        var mainWindow = new MainWindow();
+        while (mainWindow.IsLoaded)
+        {
+            await Task.Delay(100);
+        }
+
+        Close();
+        mainWindow.Show();
     }
 }
