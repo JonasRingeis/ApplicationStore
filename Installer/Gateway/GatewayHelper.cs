b9079c45b4ef3a95923b63e6b0c7ff513fd036df
JonasRingeis <jonas.ringeis@otto.de>
2025-05-09 09:50:49 +0200

    Added method summaries to gateway helper

--- Installer/Gateway/GatewayHelper.cs
+++ Installer/Gateway/GatewayHelper.cs
@@ -5,12 +5,26 @@ namespace Installer.Gateway;
 
 public static class GatewayHelper
 {
+    /// <summary>
+    /// Builds a database connection string with the base coming from an env var called DB_URI.
+    /// The env var should contain a placeholder for the db name like this: '&lt;DATABASE_NAME&gt;'.
+    /// </summary>
+    /// <param name="databaseName">The database name used for the connection string</param>
+    /// <returns>The build connection string</returns>
+    /// <exception cref="Exception">Env var 'DB_URI' is not present</exception>
     public static string GetConnectionString(string databaseName)
     {
         var templateUri = Environment.GetEnvironmentVariable("DB_URI") ?? throw new Exception("DB_URI is not set in environment variables.");
         return templateUri.Replace("<DATABASE_NAME>", databaseName);
     }
     
+    /// <summary>
+    /// Created a new sql mapper that reads the <c>[Column]</c> attributes from properties of given entity type.
+    /// If no <c>[Column]</c> attribute is set, the property name will be used. 
+    /// </summary>
+    /// <param name="caseSensitive">If <c>true</c> matches column names case-sensitive</param>
+    /// <typeparam name="T">Entity type to map</typeparam>
+    /// <exception cref="InvalidOperationException">Given Entity has no properties</exception>
     public static void UseColumnAttributeNameMapping<T>(bool caseSensitive = false)
     {
         var comparisonMethod = caseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
@@ -24,6 +38,12 @@ public static class GatewayHelper
         ));
     }
 
+    /// <summary>
+    /// Created a new sql mapper that compares the property names with entity column names and strips any underscores.
+    /// </summary>
+    /// <param name="caseSensitive">If <c>true</c> matches column names case-sensitive</param>
+    /// <typeparam name="T">Entity type to map</typeparam>
+    /// <exception cref="InvalidOperationException">Given Entity has no properties</exception>
     public static void UseStripUnderscoreMapping<T>(bool caseSensitive = false)
     {
         var comparisonMethod = caseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
