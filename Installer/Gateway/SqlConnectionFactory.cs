cac9823f3fa8ced64ea089279788b6114538e941
JonasRingeis <jonas.ringeis@otto.de>
2025-05-09 09:53:52 +0200

    Added a method summary and converted constructor into primary constructor

--- Installer/Gateway/SqlConnectionFactory.cs
+++ Installer/Gateway/SqlConnectionFactory.cs
@@ -2,15 +2,15 @@ using Microsoft.Data.SqlClient;
 
 namespace Installer.Gateway;
 
-public class SqlConnectionFactory
+public class SqlConnectionFactory(string databaseName, string? connectionString = null)
 {
-    private readonly string _connectionString;
-    
-    public SqlConnectionFactory(string databaseName, string? connectionString = null)
-    {
-        _connectionString = connectionString ?? GatewayHelper.GetConnectionString(databaseName);
-    }
+    private readonly string _connectionString = connectionString ?? GatewayHelper.GetConnectionString(databaseName);
 
+    /// <summary>
+    /// Creates a new sql connection to the database specified in the constructor.
+    /// </summary>
+    /// <param name="token">Cancellation Token for Connection</param>
+    /// <returns>The newly created SqlConnection</returns>
     public async Task<SqlConnection> CreateConnectionAsync(CancellationToken token = default)
     {
         var connection = new SqlConnection(_connectionString);
